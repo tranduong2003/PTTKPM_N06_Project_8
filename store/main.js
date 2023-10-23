9f1bd5fc87bf75e3983bb208960e38a43e8bc752
trandai03 <21011589@st.phenikaa-uni.edu.vn>
2023-10-23 19:15:21 +0700

    sửa màu nav footer

--- store/main.js
+++ store/main.js
@@ -5,15 +5,12 @@ var home = document.querySelector(".home")
 var introduce = document.querySelector(".introduce")
 
 ulList.addEventListener("click", function() {
-    ulList2.style.color = "white";
 
     introduce.style.display = "none";
     home.style.display = "block";
 });
 
 ulList2.addEventListener("click", function() {
-    this.style.color = "rgb(197, 183, 79)";
-    ulList.style.color = "white";
     
 
     introduce.style.display = "block";
